1e48aa387037d8f99a47251417c10b2a7ba8da07
t3chcrazy <abhishekprashant09@gmail.com>
2020-09-08 10:15:04 +0530

    Added all api endpoints

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
+const { assert } = require('chai');
 const port = 3000
 app.use(express.urlencoded());
 
@@ -14,6 +15,82 @@ app.use(bodyParser.json())
 // your code goes here
 
 // here
+
+app.post("/add", (req, res) => {
+    const { num1, num2 } = req.body
+    console.log(num1, num2)
+    try {
+        assert(Number.isFinite(num1) && Number.isFinite(num2), "invalid data types")
+        assert(num1 < Math.pow(10, 7) && num2 < Math.pow(10, 7), "Overflow")
+        res.json({
+            "status": "success",
+            "message": "the sum of given two number",
+            "sum": num1+num2
+        })
+    }
+    catch (err) {
+        res.json({
+            "status": "error",
+            "message": err.message,
+        })
+    }
+})
+
+app.post("/sub", (req, res) => {
+    const { num1, num2 } = req.body
+    try {
+        assert(Number.isFinite(num1) && Number.isFinite(num2), "invalid data types")
+        assert(num1 > Math.pow(10, 7) && num2 > Math.pow(10, 7), "Underflow")
+        res.json({
+            "status": "success",
+            "message": "the difference of given two number",
+            "sum": num1-num2
+        })
+    }
+    catch (err) {
+        res.json({
+            "status": "error",
+            "message": err.message,
+        })
+    }
+})
+
+app.post("/multiply", (req, res) => {
+    const { num1, num2 } = req.body
+    try {
+        assert(Number.isFinite(num1) && Number.isFinite(num2), "invalid data types")
+        assert(num1 < Math.pow(10, 7) && num2 < Math.pow(10, 7), "Overflow")
+        res.json({
+            "status": "success",
+            "message": "the product of given two number",
+            "sum": num1*num2
+        })
+    }
+    catch (err) {
+        res.json({
+            "status": "error",
+            "message": err.message,
+        })
+    }
+})
+
+app.post("/division", (req, res) => {
+    const { num1, num2 } = req.body
+    try {
+        assert(num2 != 0, "Cannot divide by zero")
+        res.json({
+            "status": "success",
+            "message": "The division of given numbers",
+            "sum": num1/num2
+        })
+    }
+    catch (err) {
+        res.json({
+            "status": "error",
+            "message": err.message
+        })
+    }
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
